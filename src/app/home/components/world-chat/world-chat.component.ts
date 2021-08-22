3c85ab0df7ce129affe7a3f5501429addcbdc6a1
shivam101gaur <gaurs3862@gmail.com>
2021-08-22 09:14:08 +0530

    ...

--- src/app/home/components/world-chat/world-chat.component.ts
+++ src/app/home/components/world-chat/world-chat.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { World } from 'src/app/models/world.model';
+import { io } from "socket.io-client";
+import { apiAddress } from 'src/environments/environment';
 
 @Component({
   selector: 'app-world-chat',
@@ -17,6 +19,13 @@ export class WorldChatComponent implements OnInit {
 
   constructor(private modalController: ModalController,) { }
 
-  ngOnInit() {  }
+  ngOnInit() { }
+
+  sayhi() {
+
+    const socket = io(apiAddress, { transports: ['websocket'] })
+    socket.connect()
+
+  }
 
 }

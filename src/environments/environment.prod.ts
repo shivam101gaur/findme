4336be32af765b8c10a21026546f9f5141cbd3d5
shivam101gaur <gaurs3862@gmail.com>
2021-09-12 22:11:09 +0530

    server url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const apiAddress:string = "https://findme-server-325808.el.r.appspot.com/"
+export const apiAddress:string = "https://findme-server-325815.el.r.appspot.com/"
 
 export const environment = {
   production: true

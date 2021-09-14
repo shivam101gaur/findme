922048bdb523092dfe7949cdb70cc6d43bb9267b
shivam101gaur <gaurs3862@gmail.com>
2021-09-14 23:53:07 +0530

    ...

--- src/app/home/components/settings/settings.component.ts
+++ src/app/home/components/settings/settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
+import { MusicControllerService } from 'src/app/services/music-controller.service';
 
 @Component({
   selector: 'app-settings',
@@ -10,7 +12,12 @@ export class SettingsComponent implements OnInit {
 
   user:User  =  JSON.parse(sessionStorage.getItem('currentUser'))
 
-  constructor() { }
+  userNameFormControl= new FormControl(this.user.name, [Validators.required])
+
+  userPasswordFormControl= new FormControl(this.user.password, [Validators.required])
+
+
+  constructor(public musicController:MusicControllerService) { }
 
   ngOnInit() {}
 

63861f88badc72ef103b2e1f3c853e6698e3fe53
shivam101gaur <47584857+shivam101gaur@users.noreply.github.com>
2021-09-21 17:39:52 +0530

    ...

--- src/app/home/components/about-app/about-app.component.ts
+++ src/app/home/components/about-app/about-app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about-app',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutAppComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController,) { }
 
   ngOnInit() {}
+
+  cancel() {
+    this.modalController.dismiss()
+  }
 
 }
